3eb28d013cd8dbb4b50d498b4ff3cd0f7db69ff5
vokod <vokoda@gmail.com>
2019-02-14 16:48:11 +0100

    added some more tests

--- settingsviews/src/androidTest/java/com/awolity/settingsviews/ButtonSettingTest.kt
+++ settingsviews/src/androidTest/java/com/awolity/settingsviews/ButtonSettingTest.kt
@@ -1,23 +1,19 @@
 package com.awolity.settingsviews
 
 import android.content.Intent
-import android.content.res.Resources
-import android.view.View
 import androidx.test.espresso.Espresso.onView
 import androidx.test.espresso.assertion.ViewAssertions.matches
 import androidx.test.espresso.matcher.ViewMatchers.*
 import androidx.test.rule.ActivityTestRule
 import androidx.test.runner.AndroidJUnit4
+import com.awolity.settingsviews.DrawableMatcher.withDrawable
 import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
-import android.widget.TextView
-import androidx.test.espresso.matcher.BoundedMatcher
 import com.awolity.settingsviews.TextColorMatcher.withTextColor
-import org.hamcrest.Description
-import org.hamcrest.Matcher
-
+import kotlinx.android.synthetic.main.activity_mock.*
+import org.hamcrest.Matchers.not
 
 @RunWith(AndroidJUnit4::class)
 class ButtonSettingTest {
@@ -41,11 +37,131 @@ class ButtonSettingTest {
     @Test
     fun testColorsSetFromAttributes() {
         restartActivity()
-        onView(withId(R.id.tv_title)).check(matches(withTextColor(
-            R.color.text_dark, activityRule.activity.resources)))
-        onView(withId(R.id.tv_desc)).check(matches(withTextColor(
-            R.color.text, activityRule.activity.resources)))
+        onView(withId(R.id.tv_title)).check(matches(withTextColor(R.color.text_dark, activityRule.activity.resources)))
+        onView(withId(R.id.tv_desc)).check(matches(withTextColor(R.color.text, activityRule.activity.resources)))
     }
+
+    @Test
+    fun testIconSetFromAttributes() {
+        restartActivity()
+        onView(withId(R.id.iv_icon)).check(matches(withDrawable(R.drawable.ic_android)))
+    }
+
+    @Test
+    fun testCheckmarkIconSetFromAttributes() {
+        restartActivity()
+        onView(withId(R.id.iv_checkmark)).check(matches(withDrawable(R.drawable.ic_check_black)))
+    }
+
+    @Test
+    fun testCheckmarkVisibleSetFromAttributes() {
+        restartActivity()
+        onView(withId(R.id.iv_checkmark)).check(matches(isDisplayed()))
+    }
+
+
+
+
+    @Test
+    fun testDisabledTitleColorFromAttribute() {
+        restartActivity()
+        activityRule.runOnUiThread { activityRule.activity.bs.isEnabled = false }
+        onView(withId(R.id.tv_title)).check(
+            matches(
+                withTextColor(
+                    R.color.text_disabled,
+                    activityRule.activity.resources
+                )
+            )
+        )
+    }
+
+    @Test
+    fun testDisabledDescriptionColorFromAttribute() {
+        restartActivity()
+        activityRule.runOnUiThread { activityRule.activity.bs.isEnabled = false }
+        onView(withId(R.id.tv_desc)).check(
+            matches(
+                withTextColor(
+                    R.color.text_disabled,
+                    activityRule.activity.resources
+                )
+            )
+        )
+    }
+
+    @Test
+    fun testDisable() {
+        restartActivity()
+        activityRule.runOnUiThread { activityRule.activity.bs.isEnabled = false }
+        activityRule.runOnUiThread { assert(!activityRule.activity.bs.isEnabled) }
+    }
+
+    @Test
+    fun testEnable() {
+        restartActivity()
+        activityRule.runOnUiThread { activityRule.activity.bs.isEnabled = false }
+        activityRule.runOnUiThread { activityRule.activity.bs.isEnabled = true }
+        activityRule.runOnUiThread { assert(!activityRule.activity.bs.isEnabled) }
+    }
+
+    @Test
+    fun testSetTitle() {
+        val title = "This is the new Title"
+        restartActivity()
+        activityRule.runOnUiThread { activityRule.activity.bs.setTitle(title) }
+        onView(withId(R.id.tv_title)).check(matches(withText(title)))
+    }
+
+    @Test
+    fun testSetDescription() {
+        val description = "This is the new description. Lorem ipsum dolor sit amet"
+        restartActivity()
+        activityRule.runOnUiThread { activityRule.activity.bs.setTitle(description) }
+        onView(withId(R.id.tv_title)).check(matches(withText(description)))
+    }
+
+    @Test
+    fun testSetIcon() {
+
+    }
+
+    @Test
+    fun testSetTitleColor() {
+    }
+
+    @Test
+    fun testSetDescriptionColor() {
+    }
+
+    @Test
+    fun testSetDisabledColor() {
+    }
+
+    @Test
+    fun testSetCheckable() {
+    }
+
+    @Test
+    fun testSetUnchekable() {
+    }
+
+    @Test
+    fun testCheckCheckable() {
+    }
+
+    @Test
+    fun testChekUnchekable() {
+    }
+
+    @Test
+    fun testUncheckCheckable() {
+    }
+
+    @Test
+    fun testUnchekUnchekable() {
+    }
+
 
     private fun restartActivity() {
         if (activityRule.activity != null) {
